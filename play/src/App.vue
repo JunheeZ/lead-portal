cf7d51e6acf09285ee17901f3faca1e15cdbe069
Junhee <zjxpdq@163.com>
2022-10-26 16:21:36 +0800

    chore: release v0.0.6

--- play/src/App.vue
+++ play/src/App.vue
@@ -1,8 +1,63 @@
 <template>
-  hahahahahhh
+  <div class="p-app">
+    <PGrid v-bind="bind" />
+  </div>
 </template>
 
 <script setup lang="ts">
+import { PGrid, ThemeEnum } from "@lead-portal/components";
+import { ref } from "vue";
+
+const bind = ref({
+  showTitle: false,
+  theme: ThemeEnum.simple,
+  columnNum: 4,
+  border: false,
+  isJump: false,
+  gutter: 0,
+  itemStyle: {
+    color: "#fff",
+    labelColor: "#fff",
+    textAlign: "center",
+    fontSize: 30,
+    width: 56,
+    height: 56,
+    background: "rgba(255, 255, 255, 0.2)",
+    radius: 18,
+    space: 12
+  },
+  wrapperBackground: "",
+  options: [
+    {
+      name: "待办事项待办事项",
+      icon: "http://static.runoob.com/images/demo/demo1.jpg",
+      url: "https://www.baidu.com",
+      count: 0
+    },
+    {
+      name: "学习地图",
+      icon: "http://static.runoob.com/images/demo/demo2.jpg",
+      url: "https://www.baidu.com",
+      count: 22
+    },
+    {
+      name: "运维工单",
+      icon: "http://static.runoob.com/images/demo/demo3.jpg",
+      url: "https://www.baidu.com",
+      count: 3333
+    },
+    {
+      name: "数据说明",
+      icon: "http://static.runoob.com/images/demo/demo4.jpg",
+      url: "https://www.baidu.com",
+      count: 4
+    }
+  ]
+});
 </script>
 
-<style scoped></style>
+<style scoped>
+.p-app {
+  background: var(--blue);
+}
+</style>
--- play/src/main.ts
+++ play/src/main.ts
@@ -1,4 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import "vant/lib/index.css";
+import "lead-portal/dist/style.css";
+import "./styles/index.less";
 
 createApp(App).mount("#app");
